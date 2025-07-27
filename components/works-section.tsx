15c0c7a4b24d7c5c5de1d4a1a42a95b6245589fb
cmliaa <cmliaqx@gmail.com>
2025-07-27 21:34:02 +0330

    Describe what you changed

--- components/works-section.tsx
+++ components/works-section.tsx
@@ -8,7 +8,7 @@ export default function WorksSection() {
      {
        id: 1,
        title: "Denja",
-       description: "A modern online store with seamless shopping experience",
+       description: "Danja is a smart rental app that aggregates listings from various platforms to offer users a seamless and faster way to find and book villas. By centralizing access across multiple services, Danja enhances convenience, saves time, and ensures users always get the best available options in one place",
        image: "/images/image_2025-07-27_16-47-29.png",
      },
     {
